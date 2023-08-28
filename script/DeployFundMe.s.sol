2bb89920d8ad0804e725825a2e59ad215868367e
Fernando <fernandito_ramirez@yahoo.com>
2023-08-28 12:48:14 -0400

    My first commit

---
+++ script/DeployFundMe.s.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+
+import {FundMe} from "../src/FoundMe.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
+
+contract DeployFundMe is Script {
+    function run() external returns (FundMe) {
+        // Befor startBroadcast -> Not a "real" tx
+        HelperConfig helperConfig = new HelperConfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig();
+        // Sapolia ETH/USD price feed address 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        // After startBroadcast -> Real tx!
+        vm.startBroadcast();
+        FundMe fundMe = new FundMe(ethUsdPriceFeed);
+        vm.stopBroadcast();
+        return fundMe;
+    }
+}
